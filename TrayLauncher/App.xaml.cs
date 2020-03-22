a74973ac9d71bbf25a0b0b2921eeaa1839241dd2
Tim Kennedy <kennedytim@outlook.com>
2020-03-22 14:32:57 -0500

    - Fixed message box disappearing when second instance is started. - Updated ChangeLog.

--- TrayLauncher/App.xaml.cs
+++ TrayLauncher/App.xaml.cs
@@ -4,6 +4,7 @@
 #region using directives
 using System.Threading;
 using System.Windows;
+using TKUtils;
 #endregion
 
 namespace TrayLauncher
@@ -13,26 +14,30 @@ namespace TrayLauncher
     /// </summary>
     public partial class App : Application
     {
-        #region Only One Instance
-        private static Mutex _mutex = null;
+        #region Change name if debugging
+#if DEBUG   // Change name when debugging
+            const string appName = "TrayLauncher_Debug";
+#else
+        const string appName = "TrayLauncher";
+#endif
+        #endregion
 
+        #region Only One Instance
         /////////////////////////////////////////////////
         // Make sure that only one instance is running //
         /////////////////////////////////////////////////
         protected override void OnStartup(StartupEventArgs e)
         {
-#if DEBUG   // Change name when debugging
-            const string appName = "TrayLauncher_Debug";
-#else
-            const string appName = "TrayLauncher";
-#endif
-            _mutex = new Mutex(true, appName, out bool createdNew);
+            _ = new Mutex(true, appName, out bool createdNew);
 
             if (!createdNew)
             {
                 //app is already running! Exiting the application
+                WriteLog.WriteTempFile("");
+                WriteLog.WriteTempFile("* An instance of TrayLauncher is already running!  Shutting this one down.");
                 _ = MessageBox.Show("An instance of TrayLauncher is already running",
-                                    "TrayLauncher", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                                    "TrayLauncher", MessageBoxButton.OK, MessageBoxImage.Exclamation,
+                                    MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
                 Voodoo.WindowsLogoffOrShutdown = true;
                 Current.Shutdown();
             }
